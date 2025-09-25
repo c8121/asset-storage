8e2fb7193808ec22fd74818d2c7bc562a62fe4d4
Christian Hau <christian@c8121.de>
2025-09-25 07:31:39 +0200

    Added build script (windows)

---
+++ build/build.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+set OUT_DIR=%~dp0..\bin
+set CMD_DIR=%~dp0..\cmd
+
+echo Build into %OUT_DIR%
+echo Sources from %OUT_DIR%
+
+go build -o %OUT_DIR%\add.exe %CMD_DIR%\add\main.go
+go build -o %OUT_DIR%\metadata-db-create.exe %CMD_DIR%\metadata-sqlite-create\main.go
+go build -o %OUT_DIR%\rest-server.exe %CMD_DIR%\rest-server\main.go
+go build -o %OUT_DIR%\spa-server.exe %CMD_DIR%\spa-server\main.go
