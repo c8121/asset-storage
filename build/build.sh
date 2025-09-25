3fba11cb053e28d03646ed7f22c52372a2f0b915
Christian Hau <ch.hau@gefro.de>
2025-09-25 11:33:26 +0200

    Added build script (linux)

---
+++ build/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASE_DIR=$(realpath "$(dirname "$0")")
+OUT_DIR=$BASE_DIR/../bin
+CMD_DIR=$BASE_DIR/../cmd
+
+echo Build into $OUT_DIR
+echo Sources from $CMD_DIR
+
+go build -o $OUT_DIR/add $CMD_DIR/add/main.go
+go build -o $OUT_DIR/metadata-db-create $CMD_DIR/metadata-sqlite-create/main.go
+go build -o $OUT_DIR/rest-server $CMD_DIR/rest-server/main.go
+go build -o $OUT_DIR/spa-server $CMD_DIR/spa-server/main.go
