a80fa92fb5de9d963e8bc0ea664037998c6531ab
Christian Hau <christian@c8121.de>
2025-09-30 09:37:03 +0200

    SPA: Added type filter

--- vue-ui/vue/components/AssetList.js
+++ vue-ui/vue/components/AssetList.js
@@ -2,9 +2,9 @@
     return {
 
         parentUrl: '/vue/components/common/BaseObjectWidget.js',
-        
+
         mixins: [
-            ui.vueMixin  
+            ui.vueMixin
         ],
 
         template: `
@@ -18,6 +18,20 @@
                                     <input id="page" v-model="page" class="form-control w-auto border-light-subtle" @changed="pageChanged" @keyup.enter="pageChanged">
                                     <button class="btn btn-outline-secondary border-light-subtle" @click="page-=(page>1?1:0);pageChanged()">&lt;</button>
                                     <button class="btn btn-outline-secondary border-light-subtle" @click="page++;pageChanged()">&gt;</button>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <div class="input-group input-group-sm">
+                                    <label for="type" class="input-group-text border-light-subtle">Type</label>
+                                    <select v-model="type" class="form-select border-light-subtle" @change="typeChanged">
+                                        <option :value="null">Any</option>
+                                        <option value="image_*">Image</option>
+                                        <option value="image_jpeg">Image: JPG</option>
+                                        <option value="image_png">Image: PNG</option>
+                                        <option value="image_gif">Image: GIF</option>
+                                        <option value="video_*">Video</option>
+                                        <option value="application_pdf">PDF</option>
+                                    </select>
                                 </div>
                             </div>
                         </div>
@@ -62,6 +76,7 @@
                 offset: 0,
                 count: 30,
                 page: 1,
+                type: null,
 
                 loading: false
             }
@@ -71,8 +86,8 @@
                 const self = this;
                 self.loading = true;
 
-                client.get(self.REST_URL + '/' + self.offset + '/' + self.count).then((json) => {
-                    if(json) {
+                client.get(self.getListUrl()).then((json) => {
+                    if (json) {
                         self.list = json;
                         self.createGroupKeys(self.list);
                     }
@@ -88,16 +103,17 @@
 
                 self.offset += self.count
 
-                client.get(self.REST_URL + '/' + self.offset + '/' + self.count).then((json) => {
-                    if(json) {
+                client.get(self.getListUrl()).then((json) => {
+                    if (json) {
                         self.createGroupKeys(json);
-                        for(const item of json)
+                        for (const item of json)
                             self.list.push(item)
                     }
                     self.page = (self.offset / self.count) + 1;
                     self.loading = false;
                 });
             },
+
             pageChanged() {
                 const self = this;
 
@@ -106,12 +122,30 @@
                 self.loadDataObject()
             },
 
+            typeChanged() {
+                const self = this;
+
+                self.list.splice(0, self.list.length);
+                self.offset = 0;
+                self.loadDataObject()
+            },
+
+            getListUrl() {
+                const self = this;
+
+                let url = self.REST_URL + '/' + self.offset + '/' + self.count;
+                if (self.type)
+                    url += '/' + self.type
+
+                return url;
+            },
+
             createGroupKeys(list) {
                 let lastGroup = "";
                 for (const asset of list) {
                     if (!asset.groupKey) {
                         asset.groupKey = asset.FileTime.substring(0, 10);
-                        if (asset.groupKey != lastGroup) {
+                        if (asset.groupKey !== lastGroup) {
                             asset.nextGroup = true;
                             lastGroup = asset.groupKey;
                         } else {
